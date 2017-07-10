d9b91036fc622cbde5e6671f82242e1134943a7f
baayso <baayso@outlook.com>
2017-07-10 14:54:04 +0800

    优化代码。

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ const userService = require('../services/user');
 
 
 router.get('/', function (req, res, next) {
-    res.send("中文最牛逼！");
+    res.send("汉语最美丽，中文最牛逼！");
 });
 
 /* GET user listing. */
