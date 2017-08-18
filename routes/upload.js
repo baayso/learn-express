51910f228d29d4326a408261ca1fc496d574cc67
baayso <baayso@outlook.com>
2017-08-18 14:39:25 +0800

    增加使用ejsExcel导出excel文件的示例。

--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const fs = require("fs");
+
 const multer = require('multer');
 const upload = multer();
 
@@ -15,8 +17,29 @@ router.post('/excel/upload', upload.single('excel'), function (req, res, next) {
     // req.file 是 `excel` 文件的信息
     // req.body 将具有文本域数据, 如果存在的话
 
-    uploadService.read(req.file.buffer, req.body.fileName, function (result) {
+    uploadService.import(req.file.buffer, req.body.fileName, function (result) {
         res.send(result);
+
+        console.log("response end.");
+    });
+
+    console.log("\nhttp request method end.");
+});
+
+router.get('/excel/download', function (req, res, next) {
+
+    uploadService.export(function (result) {
+        res.download(result, '测试导出Excel.xlsx', function (err) {
+            if (err) {
+                // Handle error, but keep in mind the response may be partially-sent
+                // so check res.headersSent
+            }
+            else {
+                // decrement a download credit, etc.
+            }
+
+            fs.unlink(result);
+        });
 
         console.log("response end.");
     });
--- services/upload.js
+++ services/upload.js
@@ -1,9 +1,12 @@
 const uploadService = {};
 
+const fs = require("fs");
+
 const ejsExcel = require('ejsexcel');
+const uuid = require('node-uuid');
 
 
-uploadService.read = function (buffer, params, callback) {
+uploadService.import = function (buffer, params, callback) {
     let exBuf = buffer;
 
     console.log("params: " + params);
@@ -25,6 +28,29 @@ uploadService.read = function (buffer, params, callback) {
     }).catch(error => {
         console.log("************** had error!");
         console.log(error);
+    });
+};
+
+uploadService.export = function (callback) {
+    let exlBuf = fs.readFileSync(__dirname + '/../template/export.xlsx');
+
+    //数据源
+    let data = [
+        [{"dpt_des": "开发部", "doc_dt": "2013-09-09", "doc": "a001"}],
+        [
+            {"pt": "pt1", "des": "des1", "due_dt": "2013-08-07", "des2": "2013-12-07"},
+            {"pt": "pt1", "des": "des1", "due_dt": "2013-09-14", "des2": "des21"}
+        ]
+    ];
+
+    //用数据源(对象)data渲染Excel模板
+    ejsExcel.renderExcel(exlBuf, data).then(function (exportBuff) {
+        let path = __dirname + '/' + uuid.v1() + '.xlsx';
+        fs.writeFileSync(path, exportBuff);
+
+        callback(path);
+    }).catch(function (err) {
+        console.error(err);
     });
 };
 
